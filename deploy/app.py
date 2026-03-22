a3e0caeed63ab948adbaad206fcdb04d3c38f0d5
Nicola Di Salvatore <nicola.disalvatore@yahoo.com>
2026-03-22 22:52:21 +0100

    Updated deploy

--- deploy/app.py
+++ deploy/app.py
@@ -1,8 +1,5 @@
-import os
-
+import uvicorn
 from api.main import app
 
 if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 7860))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
